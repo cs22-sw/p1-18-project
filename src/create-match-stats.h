d55ae4bb203fb4c75567337df2ff9eaa43d91ee3
Johan-Ravn <jran22@student.aau.dk>
2022-12-06 10:55:29 +0100

    feat: overview_matches
    CmakeFile added some things

--- src/create-match-stats.h
+++ src/create-match-stats.h
@@ -1,8 +1,12 @@
+#ifndef CREATE_MATCH_STATS_H
+#define CREATE_MATCH_STATS_H
+
 #include <string.h>
 
 #define STRING_MAX_LENGTH 256
 
-typedef struct Date {
+typedef struct
+{
     int year;
     int month;
     int day;
@@ -10,23 +14,17 @@ typedef struct Date {
     int minute;
 } Date;
 
-typedef struct Matches {
+typedef struct
+{
     char team_1[STRING_MAX_LENGTH];
     char team_2[STRING_MAX_LENGTH];
     char stadium[STRING_MAX_LENGTH];
-    struct Date match_date_info;
+    Date match_date_info;
 } Matches;
 
-Matches create_match(char *first_team, 
-                    char *second_team, 
-                    char *stadium_match, 
-                    Date info_date_match
-                    ) {
-    Matches match;
-    strcpy(match.team_1, first_team);
-    strcpy(match.team_2, second_team);
-    strcpy(match.stadium, stadium_match);
-    match.match_date_info = info_date_match;
+Matches create_match(char *first_team,
+                     char *second_team,
+                     char *stadium_match,
+                     Date info_date_match);
 
-    return match;
-}
+#endif
