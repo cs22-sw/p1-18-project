73a86dfc2598eb4d886e6b5c49bbf1b9dcf2507d
Johan-Ravn <jran22@student.aau.dk>
2022-12-06 09:55:49 +0100

    OH SHIT OH FUCK

---
+++ src/create-match-stats.c
@@ -0,0 +1,15 @@
+#include "create-match-stats.h"
+
+Matches create_match(char *first_team, 
+                    char *second_team, 
+                    char *stadium_match, 
+                    Date info_date_match
+                    ) {
+    Matches match;
+    strcpy(match.team_1, first_team);
+    strcpy(match.team_2, second_team);
+    strcpy(match.stadium, stadium_match);
+    match.match_date_info = info_date_match;
+
+    return match;
+}
--- src/create-match-stats.h
+++ src/create-match-stats.h
@@ -1,3 +1,4 @@
+
 #include <string.h>
 
 #define STRING_MAX_LENGTH 256
@@ -20,13 +21,4 @@ typedef struct Matches {
 Matches create_match(char *first_team, 
                     char *second_team, 
                     char *stadium_match, 
-                    Date info_date_match
-                    ) {
-    Matches match;
-    strcpy(match.team_1, first_team);
-    strcpy(match.team_2, second_team);
-    strcpy(match.stadium, stadium_match);
-    match.match_date_info = info_date_match;
-
-    return match;
-}
+                    Date info_date_match);
