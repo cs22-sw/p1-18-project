27a444b80451731d3e8435e4e956c32749ae5d4f
Johan-Ravn <jran22@student.aau.dk>
2022-12-07 15:13:07 +0100

    Fix: Printf output

--- src/overview-matches.c
+++ src/overview-matches.c
@@ -4,7 +4,7 @@ void overview_of_upcoming_matches(MatchList all_matches)
 {
     for (int i = 0; i < all_matches.used_size_arr; i++)
     {
-        printf("%d. \t %s\t-\t%s at %s, %d.%d %d/%d-%d \n",
+        printf("%d. %s - %s at %s, %d.%d %d/%d-%d \n",
                i,
                all_matches.match[i].team_1,
                all_matches.match[i].team_2,
